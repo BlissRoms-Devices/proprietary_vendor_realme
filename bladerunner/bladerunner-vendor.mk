8d893a27fafe62260acac7e0573deb4e3420191f
IGaganpreetSingh <gaganpannu83@gmail.com>
2023-01-08 23:26:43 +0530

    bladerunner: Import missing Display Eye Protection Data blobs
    Signed-off-by: IGaganpreetSingh <gaganpannu83@gmail.com>

--- bladerunner/bladerunner-vendor.mk
+++ bladerunner/bladerunner-vendor.mk
@@ -26,6 +26,12 @@ PRODUCT_COPY_FILES += \
     vendor/realme/bladerunner/proprietary/odm/etc/camera/settings_fdc.txt:$(TARGET_COPY_OUT_ODM)/etc/camera/settings_fdc.txt \
     vendor/realme/bladerunner/proprietary/vendor/etc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
     vendor/realme/bladerunner/proprietary/vendor/etc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    vendor/realme/bladerunner/proprietary/odm/etc/samsung1024_samsung_ams678uw01_srgb_eyeprotect_data.xml:$(TARGET_COPY_OUT_ODM)/etc/samsung1024_samsung_ams678uw01_srgb_eyeprotect_data.xml \
+    vendor/realme/bladerunner/proprietary/odm/etc/samsung1024_samsung_ams678uw01_eyeprotect_data.xml:$(TARGET_COPY_OUT_ODM)/etc/samsung1024_samsung_ams678uw01_eyeprotect_data.xml \
+    vendor/realme/bladerunner/proprietary/odm/etc/samsung1024_ana6706_cinema_eyeprotect_data.xml:$(TARGET_COPY_OUT_ODM)/etc/samsung1024_ana6706_cinema_eyeprotect_data.xml \
+    vendor/realme/bladerunner/proprietary/odm/etc/samsung1024_ana6706_colorful_eyeprotect_data.xml:$(TARGET_COPY_OUT_ODM)/etc/samsung1024_ana6706_colorful_eyeprotect_data.xml \
+    vendor/realme/bladerunner/proprietary/odm/etc/samsung1024_ana6706_eyeprotect_data.xml:$(TARGET_COPY_OUT_ODM)/etc/samsung1024_ana6706_eyeprotect_data.xml \
+    vendor/realme/bladerunner/proprietary/odm/etc/samsung1024_ana6706_srgb_eyeprotect_data.xml:$(TARGET_COPY_OUT_ODM)/etc/samsung1024_ana6706_srgb_eyeprotect_data.xml \
     vendor/realme/bladerunner/proprietary/odm/etc/sensor/config/kona_qrd_bmi26x_0.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_bmi26x_0.json \
     vendor/realme/bladerunner/proprietary/odm/etc/sensor/config/kona_qrd_cct.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_cct.json \
     vendor/realme/bladerunner/proprietary/odm/etc/sensor/config/kona_qrd_cct_rear.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_cct_rear.json \
